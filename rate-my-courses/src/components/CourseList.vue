4fa79af50b76d0368e4f773f63768acc7eb8b76c
Abul Qasim <abulqasim110@gmail.com>
2023-05-03 04:26:22 +0400

    submit review

--- rate-my-courses/src/components/CourseList.vue
+++ rate-my-courses/src/components/CourseList.vue
@@ -16,12 +16,36 @@
     </div>
   </div>
 </transition-group>
+
+<form class="addCourses">
+  <div class="form-group">
+    <label for="courseName">Course Name</label>
+    <input type="text" class="form-control" id="courseName" placeholder="Enter Course Name" v-model="coursesEra.name"/>
+  </div>
+  <div class="form-group">
+    <label for="courseDescription">Course Description</label>
+    <input type="text" class="form-control" id="courseDescription" placeholder="Enter Course Description" v-model="coursesEra.description" />
+  </div>
+  <div class="form-group">
+    <label for="courseReview">Course Review</label>
+    <input type="text" class="form-control" id="courseReview" placeholder="Enter Course Review" v-model="coursesEra.review" />
+  </div>
+  <div class="form-group">
+    <label for="courseRating">Course Rating</label>
+    <input type="number" class="form-control" id="courseRating" placeholder="Enter Course Rating"  min="0" max="5" v-model="coursesEra.rating" />
+  </div>
+  <div class="form-group">
+    <label for="courseProfessor">Course Professor</label>
+    <input type="text" class="form-control" id="courseProfessor" placeholder="Enter Course Professor" v-model="coursesEra.professor" />
+  </div>
+  <button type="button" class="btn btn-primary" @click="addCourses">Submit</button>
+</form>
 </template>
 
 <script lang="ts">
 import Course from "@/types/course";
 import OrderTerm from "@/types/orderTerm";
-import { computed, defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType, ref} from "vue";
 
 export default defineComponent({
   props: {
@@ -48,8 +72,30 @@ export default defineComponent({
         }
       });
     });
+    const coursesEra = ref<Course>({
+      name: "",
+      description: "",
+      review: "",
+      rating: 0,
+      professor: "",});
+    const addCourses = () => {
 
-    return { orderCourses };
+      // eslint-disable-next-line vue/no-mutating-props
+      props.courses.push(coursesEra.value);
+      coursesEra.value = {
+        name: "",
+        description: "",
+        review: "",
+        rating: 0,
+        professor: "",
+      };
+    };
+    return {
+      orderCourses,
+      addCourses,
+      coursesEra
+
+    };
   },
 });
 </script>
@@ -58,8 +104,15 @@ export default defineComponent({
 .list-move {
   transition: all 1s ease;
 }
+.list-enter-active {
+  transition: all 1s ease;
+}
+.list-enter-from {
+  opacity: 0;
+  transform: translateX(90px);
+}
 
-.card {
+.card, form{
   width: 90%;
   margin: 1rem auto;
   text-align: start;
@@ -84,4 +137,28 @@ export default defineComponent({
     display: flex;
     justify-content: space-between;
   }
+
+ .form-group {
+    margin: 0.5rem 0;
+  }
+
+  .form-group label {
+    margin: 0;
+  }
+
+  .form-group input {
+    width: 90%;
+    height: 2rem;
+    text-align: center;
+  }
+
+ form {
+  text-align: center;
+ }
+
+ input::placeholder {
+  color: #ccc;
+  opacity: 1;
+  text-align: center;
+ }
 </style>
